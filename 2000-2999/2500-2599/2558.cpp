7701b4471370b0951441115b43a9cf8835c59f25
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-02-01 13:49:57 +0900

    2023-02-01) in the morning

---
+++ 2000-2999/2500-2599/2558.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+}
