5456ec834f43cc1a4c17081a707d88f7c07e458b
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-02-17 17:51:51 +0900

    2023-02-17) 2 probs solved

--- 1000-1999/1600-1699/1644.cpp
+++ 1000-1999/1600-1699/1644.cpp
@@ -2,37 +2,40 @@
 
 using namespace std;
 
+const int MXN = 4000002;
+
 bool isPrime(int n) {
 	if (n == 1) { return 0; }
 	for (int i = 2; i * i <= n; i++) {
-		if (n % i == 0) { return 0; }
+		if (n % i == 0) return 0;
 	}
-	return true;
+	return 1;
 }
 
 int main() {
 	cin.tie(0);
 	cin.sync_with_stdio(0);
 
-	vector<int> v;
+	vector<int> primes;
 	int N;
 	cin >> N;
-	for (int i = 2; i <= N; i++) {
-		if (isPrime(i)) {
-			v.push_back(i);
-		}
-	}
-	
-	int solve = 0;
-	for (int st = 0; st < v.size(); st++) {
-		int en = st;
-		int sum = v[st];
 
-		while (en < v.size() && sum < N) {
-			en++;
-			if(en != v.size()) { sum += v[en]; }
-		}
+	if (N == 1) {
+		cout << 0 << '\n';
+		return 0;
+	}
+
+	for (int i = 2; i <= N; i++) {
+		if (isPrime(i))	primes.push_back(i);
+	}
+	primes.push_back(0);
+	
+	int st = 0, en = 1, solve = 0, sum = primes[0];
+	while (1) {
 		if (sum == N) { solve++; }
+		if (sum <= N) { sum += primes[en++]; }
+		if (sum > N) { sum -= primes[st++]; }
+		if (en == primes.size()) { break; }
 	}
 
 	cout << solve << '\n';
