66637f95808ccff2217b1fce168d56cf9bba7624
kim0chan <39790536+kim0chan@users.noreply.github.com>
2023-11-23 18:59:26 +0900

    solved 11719

---
+++ 11000-11999/11700-11799/11719.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+
+	string input;
+	for (int i = 0; i < 100; i ++) {
+		getline(cin, input);
+		cout << input << '\n';
+	}
+}
